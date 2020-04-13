fa47c94f76dd39a1b2adc254e77c72e0877f9da0
Evgeniy <zzzzz-123@inbox.ru>
2020-04-13 23:34:22 +0300

    refactor Plane.java

--- Clean Code task Java/Java/src/main/java/Planes/ExperimentalPlane.java
+++ Clean Code task Java/Java/src/main/java/Planes/ExperimentalPlane.java
@@ -3,7 +3,7 @@ package Planes;
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
     private ExperimentalTypes type;
     private ClassificationLevel classificationLevel;
